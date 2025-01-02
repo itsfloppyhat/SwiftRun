fb48996d6b648cf0d36fc805735cd06c8ca1e4b3
itsFloppyhat <mlucas24@icloud.com>
2025-01-01 22:11:54 -0500

    Initial Commit

---
+++ SwiftRun/SwiftRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRunApp.swift
+//  SwiftRun
+//
+//  Created by Matthew Lucas on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
