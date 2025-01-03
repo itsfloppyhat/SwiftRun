7ef5569ba4804dae829e5ae438f1c787859e0919
Matthew Lucas <121761972+itsfloppyhat@users.noreply.github.com>
2025-01-02 21:25:00 -0500

    added watch

---
+++ SwiftRunWatch Watch App/SwiftRunWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRunWatchApp.swift
+//  SwiftRunWatch Watch App
+//
+//  Created by Matthew Lucas on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRunWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
